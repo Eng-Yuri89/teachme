20b8420bb98aef7a66558d8128cadadf7f174769
JavierCabrera1 <67341767+JavierCabrera1@users.noreply.github.com>
2020-06-28 11:31:04 -0600

    Finish the favorite screen

--- lib/ui/screens/home/favorite_sreen.dart
+++ lib/ui/screens/home/favorite_sreen.dart
@@ -1,4 +1,6 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:teachme/models/teacher.dart';
 import 'package:teachme/utils/size.dart';
 
 class FavoriteScreen extends StatefulWidget {
@@ -9,6 +11,64 @@ class FavoriteScreen extends StatefulWidget {
 class _FavoriteScrenState extends State<FavoriteScreen> {
   static ThemeData _theme;
 
+  //Favorite list.
+  List<Teacher> _topList = [
+    Teacher(
+      id: "123",
+      email: "angelcabrera18398@gmail.com",
+      fullname: "Angel Cabrera",
+      phoneNumber: "+502 30435391",
+      description: "Software Engineer, 5 years mobile development experience.",
+      photoUrl:
+          "https://i.pinimg.com/originals/73/d3/1c/73d31c1a205363fa75e8c8834d3f1166.jpg",
+    ),
+    Teacher(
+      id: "125",
+      email: "juanperez@gmail.com",
+      fullname: "Juan Perez",
+      phoneNumber: "+502 30344591",
+      description: "Software Engineer, 5 years mobile development experience.",
+      photoUrl:
+          "https://i.pinimg.com/originals/73/d3/1c/73d31c1a205363fa75e8c8834d3f1166.jpg",
+    ),
+    Teacher(
+      id: "127",
+      email: "pedrosalazar@gmail.com",
+      fullname: "Pedro Salazar",
+      phoneNumber: "+502 98765434",
+      description: "Software Engineer, 5 years mobile development experience.",
+      photoUrl:
+          "https://i.pinimg.com/originals/73/d3/1c/73d31c1a205363fa75e8c8834d3f1166.jpg",
+    ),
+    Teacher(
+      id: "125",
+      email: "juanperez@gmail.com",
+      fullname: "Juan Perez",
+      phoneNumber: "+502 30344591",
+      description: "Software Engineer, 5 years mobile development experience.",
+      photoUrl:
+          "https://i.pinimg.com/originals/73/d3/1c/73d31c1a205363fa75e8c8834d3f1166.jpg",
+    ),
+    Teacher(
+      id: "125",
+      email: "juanperez@gmail.com",
+      fullname: "Juan Perez",
+      phoneNumber: "+502 30344591",
+      description: "Software Engineer, 5 years mobile development experience.",
+      photoUrl:
+          "https://i.pinimg.com/originals/73/d3/1c/73d31c1a205363fa75e8c8834d3f1166.jpg",
+    ),
+    Teacher(
+      id: "125",
+      email: "juanperez@gmail.com",
+      fullname: "Juan Perez",
+      phoneNumber: "+502 30344591",
+      description: "Software Engineer, 5 years mobile development experience.",
+      photoUrl:
+          "https://i.pinimg.com/originals/73/d3/1c/73d31c1a205363fa75e8c8834d3f1166.jpg",
+    ),
+  ];
+
   TextEditingController _filterController = new TextEditingController();
 
   @override
@@ -17,8 +77,11 @@ class _FavoriteScrenState extends State<FavoriteScreen> {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             _filterInput(context),
+            _optionHeader("Favorites", context),
+            _topListView(context),
           ],
         ),
       ),
@@ -79,6 +142,145 @@ class _FavoriteScrenState extends State<FavoriteScreen> {
           ],
         ),
       ),
+    );
+  }
+
+  ///Option header creation
+  Widget _optionHeader(String title, BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: screenAwareWidth(30, context)),
+      child: Text(
+        title,
+        style: _theme.textTheme.headline6.copyWith(
+          color: _theme.backgroundColor,
+        ),
+      ),
+    );
+  }
+
+  /// Returns the favorites list view
+  /// separated.
+  Widget _topListView(BuildContext context) {
+    return Expanded(
+      child: ListView.separated(
+        scrollDirection: Axis.vertical,
+        padding: EdgeInsets.all(screenAwareWidth(20, context)),
+        itemCount: _topList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _topCard(context, _topList[index]);
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider(color: _theme.backgroundColor.withOpacity(0.25));
+        },
+      ),
+    );
+  }
+
+  /// The favorite teacher returns
+  /// card with relevant information.
+  Widget _topCard(BuildContext context, Teacher teacher) {
+    return Row(
+      children: <Widget>[
+        //Teacher image.
+        _topCardImage(context, teacher),
+        SizedBox(width: screenAwareWidth(15, context)),
+        Container(
+          height: screenAwareHeight(100, context),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              //Header card.
+              _topCardHeader(context, teacher),
+              //Teacher description.
+              Container(
+                height: screenAwareHeight(38, context),
+                width: screenAwareWidth(260, context),
+                child: AutoSizeText(
+                  teacher.description,
+                  style: _theme.textTheme.bodyText2.copyWith(
+                    color: _theme.backgroundColor.withOpacity(0.8),
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              _topCartFooter(context, teacher),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  ///Return the favorite card image.
+  Widget _topCardImage(BuildContext context, Teacher teacher) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(screenAwareWidth(5, context)),
+      child: FadeInImage(
+        fit: BoxFit.fill,
+        height: screenAwareHeight(100, context),
+        width: screenAwareWidth(60, context),
+        placeholder: AssetImage("assets/home/loading.gif"),
+        image: NetworkImage(teacher.photoUrl),
+      ),
+    );
+  }
+
+  ///Returns the teacher's name and the
+  ///favorite button add.
+  Widget _topCardHeader(BuildContext context, Teacher teacher) {
+    return Container(
+      width: screenAwareWidth(260, context),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          //Teacher name.
+          Text(
+            teacher.fullname,
+            style: _theme.textTheme.subtitle1.copyWith(
+              color: _theme.backgroundColor,
+            ),
+          ),
+          //Favorite button.
+          Image.asset(
+            "assets/landing/favorite.png",
+            fit: BoxFit.fill,
+            width: screenAwareWidth(16, context),
+          )
+        ],
+      ),
+    );
+  }
+
+  ///Returns the teacher rated and the
+  ///number of classes.
+  Widget _topCartFooter(BuildContext context, Teacher teacher) {
+    return Row(
+      children: <Widget>[
+        //Rated.
+        Text(
+          "4.5",
+          style: _theme.textTheme.subtitle2.copyWith(
+            color: _theme.backgroundColor,
+          ),
+        ),
+        SizedBox(width: screenAwareWidth(3, context)),
+        //Star icon.
+        Icon(
+          Icons.star,
+          size: screenAwareWidth(16, context),
+          color: Color.fromRGBO(66, 99, 235, 1),
+        ),
+        SizedBox(width: screenAwareWidth(6, context)),
+        //Class number.
+        Text(
+          "125 Classes",
+          style: _theme.textTheme.caption.copyWith(
+            color: _theme.backgroundColor.withOpacity(0.8),
+          ),
+        ),
+      ],
     );
   }
 }
