d1a4baa36f193b1fecdf0a78021f691ace99a3b9
Angel Gabriel Cabrera Guzman <acabrera@entaeh.local>
2020-06-28 23:38:33 -0600

    Delete dot in messages

--- lib/ui/screens/home/favorite_screen.dart
+++ lib/ui/screens/home/favorite_screen.dart
@@ -202,7 +202,7 @@ class _FavoriteScrenState extends State<FavoriteScreen> {
                         } else {
                           return Center(
                             child: Text(
-                              "There are no favorite teachers yet.",
+                              "There are no favorite teachers yet",
                               style: _theme.textTheme.bodyText2.copyWith(
                                 color: _theme.accentColor.withOpacity(0.8),
                               ),
@@ -225,7 +225,7 @@ class _FavoriteScrenState extends State<FavoriteScreen> {
                 } else {
                   return Center(
                     child: Text(
-                      "There are no favorite teachers yet.",
+                      "There are no favorite teachers yet",
                       style: _theme.textTheme.bodyText2.copyWith(
                         color: _theme.accentColor.withOpacity(0.8),
                       ),
